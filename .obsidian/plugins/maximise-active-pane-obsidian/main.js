d87aff1ea1fc3c793641505caa1994777f2d231c
foreveryone1 <afif.hasan96@gmail.com>
2022-10-16 00:45:41 +0200

    vault backup: 2022-10-16 00:45:41

--- .obsidian/plugins/maximise-active-pane-obsidian/main.js
+++ .obsidian/plugins/maximise-active-pane-obsidian/main.js
@@ -100,13 +100,20 @@ class MaximiseActivePanePlugin extends PluginBase {
             hotkeys: [{ modifiers: ['Mod', 'Shift'], key: 'x' }],
             callback: () => {
                 // simply toggle the 'maximised' class and let the css do its thing
+				let removemaximisedparentsplit = Array.from(
+				this.app.workspace.rootSplit.containerEl.querySelectorAll(".maximisedparentsplit")
+				);
+				removemaximisedparentsplit.forEach((node) => {
+				  node.removeClass("maximisedparentsplit");
+				});
                 this.rootSplit.containerEl.toggleClass('maximised', !this.rootSplit.containerEl.hasClass('maximised'));
                 if (this.app.workspace.activeLeaf) {
                     let i = this.app.workspace.activeLeaf.containerEl.parentNode;
                     for (; i != this.rootSplit.containerEl; i = i.parentNode) {
                         i.toggleClass('maximisedparentsplit', !i.hasClass('maximisedparentsplit'));
                     }
-                }
+                };
+				
                 this.app.workspace.onLayoutChange();
             }
         });
